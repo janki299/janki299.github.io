e19d9be276fe74dc4088c9c09a511980ce590004
janki bhatt <jankibhatt299@gmail.com>
2025-05-05 12:12:34 +0530

    index changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4127065710"
+    serviceWorkerVersion: "2916005625"
   }
 });
